fb601cdcd07720f25acbff243c1d4dc5dd6bfed6
Laura Pinto <laura.pinto@yomp.pt>
2019-06-17 19:16:05 +0100

    Updates

---
+++ precache-manifest.5f6edcb566e6380f19b10e32b9142e13.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa8d572584ba593ce96b82fb3d3ec4e1",
+    "url": "/insecta/static/media/foto1.aa8d5725.jpeg"
+  },
+  {
+    "revision": "8a795000ca7c2e8827c7",
+    "url": "/insecta/static/css/main.f72543b4.chunk.css"
+  },
+  {
+    "revision": "1c78d3374493d7bfcbaa",
+    "url": "/insecta/static/js/runtime~main.16f01d99.js"
+  },
+  {
+    "revision": "d3ac3cb255af03155181",
+    "url": "/insecta/static/js/2.7cff1888.chunk.js"
+  },
+  {
+    "revision": "05e07a4f98b591ae626d056ed2dacb2e",
+    "url": "/insecta/static/media/cienciaviva.05e07a4f.png"
+  },
+  {
+    "revision": "f012024e17d0f0bb851dd555ea685b5e",
+    "url": "/insecta/static/media/mhnc.f012024e.png"
+  },
+  {
+    "revision": "480fb6e4102c89a45077191d1adb5de0",
+    "url": "/insecta/static/media/zoosanto.480fb6e4.jpg"
+  },
+  {
+    "revision": "8cf783594145221562ef644abf54dfa1",
+    "url": "/insecta/static/media/parquebio.8cf78359.jpg"
+  },
+  {
+    "revision": "bcf1dc95f01d2ac5a0d3fd9f0bbaa80d",
+    "url": "/insecta/static/media/IPP.bcf1dc95.png"
+  },
+  {
+    "revision": "8a795000ca7c2e8827c7",
+    "url": "/insecta/static/js/main.94caccd2.chunk.js"
+  },
+  {
+    "revision": "7141c3c332ad16fb92b1c0a8f579a3f7",
+    "url": "/insecta/static/media/foto3.7141c3c3.jpeg"
+  },
+  {
+    "revision": "c8ef298c07f876dfefa84fc08ce52c0d",
+    "url": "/insecta/static/media/luis3.c8ef298c.jpeg"
+  },
+  {
+    "revision": "17bfdcfda8d9aa9c74d232ad723f073d",
+    "url": "/insecta/static/media/foto4.17bfdcfd.jpeg"
+  },
+  {
+    "revision": "ca119e85cc1e16f15c6f58c47c7ede23",
+    "url": "/insecta/static/media/foto2.ca119e85.jpeg"
+  },
+  {
+    "revision": "08a5464ff206fb665f941df89017b760",
+    "url": "/insecta/static/media/zoomaia.08a5464f.jpg"
+  },
+  {
+    "revision": "83eba16366b03dfdba030372119c9829",
+    "url": "/insecta/static/media/esmaequadrado.83eba163.png"
+  },
+  {
+    "revision": "61bca8f0a3d1bddbeb111ae7d909dc51",
+    "url": "/insecta/static/media/serralves.61bca8f0.jpg"
+  },
+  {
+    "revision": "872fdeebcff58652e09d9d75b235f8d5",
+    "url": "/insecta/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.007eed1a34ae2b28b6310f58f81923c0.js"
+  "/insecta/precache-manifest.5f6edcb566e6380f19b10e32b9142e13.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/insecta/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
